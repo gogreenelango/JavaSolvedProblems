6d875f3bd6dcb08ea1ce966de0cca77c999f5531
gogreenelango <elangovansubramanian98@gmail.com>
2018-06-19 17:18:01 +0530

    StringToDouble

---
+++ StringToDouble.java
@@ -0,0 +1,23 @@
+package com.array;
+
+
+public class StringToDouble { 
+
+	public static void main(String[] args) 
+ 
+
+        {    
+
+            double s1=Double.parseDouble(args[0]);
+            double s2=Double.parseDouble(args[1]);
+             double total=s1+s2;
+             System.out.println(total);
+            
+            
+            
+
+        } 
+
+    }
+
+
