bf2b48234a5c1e4a9535570185c65f67e876d285
gogreenelango <39374061+gogreenelango@users.noreply.github.com>
2018-06-19 19:00:07 +0530

    Update NumberSign.java

--- NumberSign.java
+++ NumberSign.java
@@ -1,8 +1,9 @@
+import java.util.Scanner;
 class NumberSign{
 public static void main(String args[]){
 
-
-int input=3;
+Scanner sc=new Scanner(System.in);
+int input=sc.nextInt;
 if(input>0) System.out.println("Postive");
 else if(input==0) System.out.println("Zero");
 else System.out.println("Negative");
