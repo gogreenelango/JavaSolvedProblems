f099052c37e343a1e16a4528032f1ffb5c65c2f5
gogreenelango <39374061+gogreenelango@users.noreply.github.com>
2018-06-19 18:57:00 +0530

    Create NumberSign.java

---
+++ NumberSign.java
@@ -0,0 +1,10 @@
+class NumberSign{
+public static void main(String args[]){
+
+
+int input=3;
+if(input>0) System.out.println("Postive");
+else if(input==0) System.out.println("Zero");
+else System.out.println("Negative");
+
+}}
